0ac632eaae402e245d02732ef09c22562220b03b
Lukas Tomasek <52459545+lukastomasek@users.noreply.github.com>
2020-11-19 19:53:50 -0500

    testing vuforia ar

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    [SerializeField] Animator anim;
+
+    const string fight_id = "attack_1";
+
     private void Start()
     {
         Debug.Log("<color=green> Testing </color>");
+
+        if (anim == null)
+        {
+            anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        }
     }
+
+    public void PlayFightAnimation() => anim.Play(fight_id);
+   
 }

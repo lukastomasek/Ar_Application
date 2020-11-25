1cdb16516b06ae00366196966f8cab8f2e50a9ad
Lukas Tomasek <52459545+lukastomasek@users.noreply.github.com>
2020-11-25 15:47:12 -0500

    added new fonts and created loading screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class GameController : MonoBehaviour
 {
-
     [SerializeField] Animator anim;
     [SerializeField] GameObject effect;
 
---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] float loadingTimer;
+    [SerializeField] bool useAsync = false;
+    [SerializeField] GameObject parent;
+    [SerializeField] Image loadingBar;
+    [SerializeField] int scene;
+    float _timer = 0f;
+    AudioSource[] _audios;
+
+    private void Start()
+    {
+        _audios = FindObjectsOfType<AudioSource>();
+
+        StartCoroutine(StartLoading());
+    }
+
+
+    IEnumerator StartLoading()
+    {
+        if (loadingTimer == 0)
+            loadingTimer = 0.5f;
+
+        if (useAsync)
+        {
+            AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+
+            if (ao == null)
+                Debug.LogError($"missing scene : {scene} ");
+
+            while (!ao.isDone)
+            {
+                float timer = ao.progress / 0.9f;
+
+                loadingBar.fillAmount = timer;
+
+                yield return null;
+            }
+        }
+        else
+        {
+            parent.SetActive(true);
+
+            if (_audios.Length != 0)
+            {
+                foreach (var a in _audios)
+                    a.mute = true;
+            }
+
+            Debug.Log("loading");
+            while (_timer < 1)
+            {
+                _timer += Time.deltaTime * loadingTimer;
+
+                loadingBar.fillAmount = _timer;
+                yield return null;
+
+                if (_timer > 1)
+                {
+                    if (_audios.Length != 0)
+                        foreach (var a in _audios)
+                            a.mute = false;
+
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
